4dedfcce7b56cc2f73262ec82c3f6241cd18ae0f
Claudinei Felix <claudinei.felix@outlook.com>
2023-02-08 22:45:54 -0300

    Aula 11 - Criando uma Máquina de estado para as reservas

--- BookingService/Core/Domain/Entitities/Booking.cs
+++ BookingService/Core/Domain/Entitities/Booking.cs
@@ -4,10 +4,23 @@ namespace Domain.Entitities
 {
     public class Booking
     {
-        public Guid? Id { get; set;}
+        public Guid? Id { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         private Status Status { get; set; }
         public Status GetCurretStaus { get { return Status; } }
+
+        public void ChangeState(Actions action)
+        {
+            Status = (Status, action) switch
+            {
+                (Status.Created, Actions.Pay) => Status.Paid,
+                (Status.Created, Actions.Cancel) => Status.Canceled,
+                (Status.Paid, Actions.Finish) => Status.Finished,
+                (Status.Paid, Actions.Refound) => Status.Refounded,
+                (Status.Canceled, Actions.Reopen) => Status.Created,
+                _ => Status
+            };
+        }
     }
 }
---
+++ BookingService/Core/Domain/Enums/Actions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Actions
+    {
+        Pay,
+        Finish, // Comprou e usou
+        Cancel, // Nunca foi pago
+        Refound, // Pago e retornado
+        Reopen // Cancelado
+    }
+}
