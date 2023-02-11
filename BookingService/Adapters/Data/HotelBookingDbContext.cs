df0fab93f3d2031bf674ce4a2d24f16cf3122a50
Claudinei Felix <claudinei.felix@outlook.com>
2023-02-10 22:21:43 -0300

    Aula 13 - Conectando com banco de dados e habilitando as migrations

---
+++ BookingService/Adapters/Data/HotelBookingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entitities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class HotelBookingDbContext: DbContext
+    {
+        public HotelBookingDbContext(DbContextOptions<HotelBookingDbContext> options): base(options) { }
+
+        DbSet<Guest> Guests { get; set; }
+        DbSet<Booking> Bookings { get; set; }
+        DbSet<Room> Rooms { get; set; }
+    }
+}
--- BookingService/Comsumers/API/Program.cs
+++ BookingService/Comsumers/API/Program.cs
@@ -1,8 +1,18 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+
+# region Conexao ao banco de dados
+var connectionString = builder.Configuration.GetConnectionString("sqlConnection");
+builder.Services.AddDbContext<HotelBookingDbContext>(options => options.UseSqlServer(connectionString));
+# endregion
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
